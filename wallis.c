53d99410ab93be9d8288d17187406ff3f41db5d2
BalamuruganV6 <84074716+BalamuruganV6@users.noreply.github.com>
2021-05-31 23:01:06 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -26,8 +26,9 @@ int main(void) {
 float wallis_pi(int x)
 {
     float a = 1.0;
-    for(int i=1;i<=x;i++)
+    for(int i=1;i<=x;i++){
         a = a*4*i*i/((4*i*i)-1);
+    }
     return 2*a;
 }
 
